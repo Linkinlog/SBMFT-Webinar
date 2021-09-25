323c5bc47bc2e527b3a871c192c3e78e22adce18
Dahlton <dahlton@linkd.dev>
2021-09-24 22:17:42 -0400

    set up main and single video components

---
+++ src/components/videos/SingleVideo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SingleVideo = ({video}) => {
+	return (
+		<div>
+			<div className='card text-center videoCard'>
+				<img src={video.imageURL} className='card-img-top' alt='...' />
+				<div className='card-body'>
+					<h5 className='card-title'>{video.title}</h5>
+					<h6 className='card-subtitle mb-2 text-muted '>Video Description</h6>
+					<p className='card-text'>{video.desc.substring(0, 40)}...</p>
+					<a href=" "><img src="https://img.icons8.com/bubbles/50/000000/play.png"/></a>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ src/components/videos/VideosMain.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { SingleVideo } from './SingleVideo'
+
+export const VideosMain = () => {
+	const [video, setVideo] = useState([
+		{
+			title: 'Fancy catchy unique title',
+			desc : 'words that are describing the video that will enrich your life. ',
+			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+		},
+		{
+			title: 'Fancy catchy unique title',
+			desc : 'words that are describing the video that will enrich your life. ',
+			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+		},
+		{
+			title: 'Fancy catchy unique title',
+			desc : 'words that are describing the video that will enrich your life. ',
+			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+		},
+		{
+			title: 'Fancy catchy unique title',
+			desc : 'words that are describing the video that will enrich your life. ',
+			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+		},
+		{
+			title: 'Fancy catchy unique title',
+			desc : 'words that are describing the video that will enrich your life. ',
+			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+		}
+	])
+	console.log(video);
+	return (
+		<div className='container row row-cols-2 row-cols-md-4 g-2 pt-4'>
+			{video.map(vid => <SingleVideo video={vid} />)}
+		</div>
+	)
+}
