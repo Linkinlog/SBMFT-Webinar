9159304fa776b3d6c01640b16f7edbff83ad65b2
Dahlton <dahlton@linkd.dev>
2021-09-24 22:17:28 -0400

    started on home page

---
+++ src/components/layout/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import { VideosMain } from '../videos/VideosMain'
+
+export const Home = () => {
+	return (
+		<Fragment>
+			<VideosMain />
+		</Fragment>
+	)
+}
