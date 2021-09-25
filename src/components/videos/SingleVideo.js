7c65b1fb114daa643cb3a77eb354b955758919e7
Linkinlog <dahlton@linkd.dev>
2021-09-24 23:42:02 -0400

    added alt to fix error

--- src/components/videos/SingleVideo.js
+++ src/components/videos/SingleVideo.js
@@ -4,12 +4,12 @@ export const SingleVideo = ({video}) => {
 	return (
 		<div>
 			<div className='card text-center videoCard'>
-				<img src={video.imageURL} className='card-img-top' alt='...' />
+				<img src={video.imageURL} className='card-img-top' alt='Video' />
 				<div className='card-body'>
 					<h5 className='card-title'>{video.title}</h5>
 					<h6 className='card-subtitle mb-2 text-muted '>Video Description</h6>
 					<p className='card-text'>{video.desc.substring(0, 40)}...</p>
-					<a href=" "><img src="https://img.icons8.com/bubbles/50/000000/play.png"/></a>
+					<a href=" "><img src="https://img.icons8.com/bubbles/50/000000/play.png" alt='play'/></a>
 				</div>
 			</div>
 		</div>
--- src/components/videos/VideosMain.js
+++ src/components/videos/VideosMain.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { SingleVideo } from './SingleVideo'
 
 export const VideosMain = () => {
-	const [video, setVideo] = useState([
+	const [video] = useState([
 		{
 			title: 'Fancy catchy unique title',
 			desc : 'words that are describing the video that will enrich your life. ',
@@ -29,7 +29,6 @@ export const VideosMain = () => {
 			imageURL : 'https://images.unsplash.com/photo-1626609572015-420cd41eddb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
 		}
 	])
-	console.log(video);
 	return (
 		<div className='container row row-cols-2 row-cols-md-4 g-2 pt-4'>
 			{video.map(vid => <SingleVideo video={vid} />)}
