e44e899d699208528a51ebf5e5df272ab43d8364
Linkinlog <dahlton@linkd.dev>
2021-09-26 22:26:58 -0400

    videoCard : made clickable rather than button

--- src/components/videos/SingleVideo.js
+++ src/components/videos/SingleVideo.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export const SingleVideo = ({video}) => {
+export const SingleVideo = ({ video }) => {
 	return (
-		<div>
-			<div className='card text-center videoCard'>
-				<img src={video.imageURL} className='card-img-top' alt='Video' />
-				<div className='card-body'>
-					<h5 className='card-title'>{video.title}</h5>
-					<h6 className='card-subtitle mb-2 text-muted '>Video Description</h6>
-					<p className='card-text'>{video.desc.substring(0, 40)}...</p>
-					<a href=" "><img src="https://img.icons8.com/bubbles/50/000000/play.png" alt='play'/></a>
+		<div key={video.id} className='col my-4'>
+			<a href={`/video/${video.id}`} className='videoCard'>
+				<div className='card pb-1 text-center videoCard shadow1 rounded rounded-3'>
+					<img src={video.imageURL} className='card-img-top shadow1 rounded rounded-3' alt='Video' />
+					<div className='card-body'>
+						<h5 className='card-title'>{video.title}</h5>
+						<p className='card-text'>{video.desc.substring(0, 40)}...</p>
+					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 	);
-}
+};
