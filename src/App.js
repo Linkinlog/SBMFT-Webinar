90a8e1703e0861439c158f647d8b3eb1011f68b9
Linkinlog <dahlton@linkd.dev>
2021-09-26 22:27:20 -0400

    added authContext to main app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/layout/Home';
+import { AuthPage } from './components/layout/AuthPage';
+import AuthState from './context/auth/AuthState';
+
 const App = () => {
 	return (
-		<Router>
-			<Navbar />
-			<div className='container'>
-				<Switch>
-					<Route exact path='/' component={Home} />
-				</Switch>
-			</div>
-		</Router>
+		<AuthState>
+			<Router>
+				<Navbar />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/auth' component={AuthPage} />
+					</Switch>
+				</div>
+			</Router>
+		</AuthState>
 	);
 };
 
