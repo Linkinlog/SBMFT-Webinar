94e08ec528b664dac26055649ae04a98e666f53e
Linkinlog <dahlton@linkd.dev>
2021-09-29 00:03:03 -0400

    Nav: Changed Sign Up/In/Out buttons based on auth

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,22 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { app } from '../../Config';
 import AuthContext from '../../context/auth/authContext';
 
 export const Navbar = () => {
-	const authContext = useContext(AuthContext)
-	const { login } = authContext;
-	console.log(app.auth());
-	if(app.auth().currentUser){
-		login(app.auth().currentUser.providerData[0])
-	}
+	const authContext = useContext(AuthContext);
+	const { isAuthenticated } = authContext;
+
 	return (
 		<nav className='navbar navbar-expand-lg navbar-dark bg-light'>
 			<div className='container-fluid'>
-				<Link className='navbar-brand ' to='/' >
-					Courtney Seyler M.A, LMFT
+				<Link className='navbar-brand ' to='/'>
+					Courtney Seyler M.A, Licensed Marriage & Family Therapist
 				</Link>
 				<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
 					<span className='navbar-toggler-icon'></span>
 				</button>
 				<div className='collapse navbar-collapse' id='navbarNav'>
-					<ul className='navbar-nav'>
+					<ul className='navbar-nav ms-auto'>
 						<li className='nav-item'>
 							<Link className='nav-link' to='/'>
 								Home
@@ -32,6 +28,28 @@ export const Navbar = () => {
 							</Link>
 						</li>
 					</ul>
+					{isAuthenticated ? (
+						<ul className='navbar-nav'>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/logout'>
+									Log Out
+								</Link>
+							</li>
+						</ul>
+					) : (
+						<ul className='navbar-nav'>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/login'>
+									Log In
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/signup'>
+									Sign Up
+								</Link>
+							</li>
+						</ul>
+					)}
 				</div>
 			</div>
 		</nav>
