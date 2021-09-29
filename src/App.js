27d1fbcd5c40646433c52a3e90b7d96b0d3c12ee
Linkinlog <dahlton@linkd.dev>
2021-09-28 23:57:07 -0400

    app router: added signup/in/out

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/layout/Home';
-import { AuthPage } from './components/layout/AuthPage';
+import { SignUp } from './components/layout/SignUp';
 import AuthState from './context/auth/AuthState';
+import { Login } from './components/layout/Login';
+import { Logout } from './components/layout/Logout';
 
 const App = () => {
 	return (
@@ -13,7 +15,9 @@ const App = () => {
 				<div className='container'>
 					<Switch>
 						<Route exact path='/' component={Home} />
-						<Route exact path='/auth' component={AuthPage} />
+						<Route exact path='/signup' component={SignUp} />
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/logout' component={Logout} />
 					</Switch>
 				</div>
 			</Router>
