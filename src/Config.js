a0c38456df0dbd01c8cf93d01ddd7d24d973560d
Linkinlog <dahlton@linkd.dev>
2021-10-01 01:28:12 -0400

    firebase: got new keys and put into dotenv

--- src/Config.js
+++ src/Config.js
@@ -1,31 +1,17 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app';
+import 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyBi-jR4EdInfwUSbACd0PSNwRa9QRQNKaY',
-	authDomain: 'sbmft-webinars.firebaseapp.com',
-	projectId: 'sbmft-webinars',
-	storageBucket: 'sbmft-webinars.appspot.com',
-	messagingSenderId: '1080989994907',
-	appId: '1:1080989994907:web:f38f4d67a106774fefdb3e',
-	measurementId: 'G-KRGVQL9C0F',
-};
-// Configure FirebaseUI.
-export const uiConfig = {
-	// Popup signin flow rather than redirect flow.
-	signInFlow: 'popup',
-	// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-	callbacks: {
-		// Avoid redirects after sign-in.
-		signInSuccessWithAuthResult: () => false,
-	},
-	// We will display Google and Facebook as auth providers.
-	signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+	apiKey: process.env.REACT_APP_FIREBASE_API,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_FIREBASE_APPID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
-export const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth()
