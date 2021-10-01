b7af08d37a588e264befd6d04e271bf47a9ddb96
Linkinlog <dahlton@linkd.dev>
2021-10-01 01:03:40 -0400

    Changed navbar text

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
 		<nav className='navbar navbar-expand-lg navbar-dark bg-light'>
 			<div className='container-fluid'>
 				<Link className='navbar-brand ' to='/'>
-					Courtney Seyler M.A, Licensed Marriage & Family Therapist
+					Courtney Seyler M.A., Licensed Marriage & Family Therapist
 				</Link>
 				<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
 					<span className='navbar-toggler-icon'></span>
