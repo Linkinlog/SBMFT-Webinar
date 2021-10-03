9acdc4aaa563270e57c52e6dcd2650f25d54a3b7
Linkinlog <dahlton@linkd.dev>
2021-10-03 18:24:21 -0400

    Videos: moved main to all using context

---
+++ src/components/videos/AllVideos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SingleVideo } from './SingleVideo';
+
+const AllVideos = ({ videos }) => {
+	return (
+		<>
+			{videos.map((vid) => (
+				<SingleVideo video={vid} key={vid.id} />
+			))}
+		</>
+	);
+};
+
+export default AllVideos;
