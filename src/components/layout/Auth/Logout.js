c271db6920f610a97b1656b9a8988b8fc31cd5da
Linkinlog <dahlton@linkd.dev>
2021-10-03 18:30:03 -0400

    Auth: Made logout component look like rest

--- src/components/layout/Auth/Logout.js
+++ src/components/layout/Auth/Logout.js
@@ -1,31 +1,39 @@
-import React, { useContext, useEffect } from 'react'
-import { Button, Card } from 'react-bootstrap'
-import { useHistory } from 'react-router'
-import AuthContext from '../../../context/auth/authContext'
+import React, { useContext, useEffect } from 'react';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import AuthContext from '../../../context/auth/authContext';
 
 export const Logout = () => {
-	const authContext = useContext(AuthContext)
+	const authContext = useContext(AuthContext);
 	const { logout, isAuthenticated } = authContext;
-	const history = useHistory()
-	const onClick = e => {
-		e.preventDefault()
-		logout()
-	}
+	const history = useHistory();
+	const onClick = (e) => {
+		e.preventDefault();
+		logout();
+	};
 
 	useEffect(() => {
-		!isAuthenticated && history.push('/')
+		!isAuthenticated && history.push('/');
 		// eslint-disable-next-line
 	}, [isAuthenticated]);
-	
+
 	return (
-		<>
-			<Card className='mt-4 text-center'>
-				<Card.Body>
-					<Card.Title><h2>Log Out</h2></Card.Title>
-					<Card.Text>Click below to sign out</Card.Text>
-					<Button variant='primary' onClick={onClick} >Log Out</Button>
-				</Card.Body>
-			</Card>
-		</>
-	)
-}
+		<Container>
+			<Row>
+				<Col md={6} className='offset-md-3'>
+					<Card className='mt-4 text-center'>
+						<Card.Body>
+							<Card.Title>
+								<h2>Log Out</h2>
+							</Card.Title>
+							<Card.Text>Click below to sign out</Card.Text>
+							<Button variant='primary' onClick={onClick}>
+								Log Out
+							</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
