6f1f6dcc3d8f463f25223649603b28729ea04d1a
Linkinlog <dahlton@linkd.dev>
2021-10-03 18:23:20 -0400

    VideoContext: added initial vids

--- src/context/video/VideoState.js
+++ src/context/video/VideoState.js
@@ -1,22 +1,88 @@
-import React, { useReducer } from 'react'
-import VideoContext from './videoContext'
-import videoReducer from './videoReducer'
-const VideoState = ({children}) => {
+import React, { useReducer } from 'react';
+import VideoContext from './videoContext';
+import videoReducer from './videoReducer';
+
+const VideoState = ({ children }) => {
 	const initialState = {
-		// todo set up state
-	}
-	const [state, disptatch] = useReducer(videoReducer, initialState)
-	
+		videos: [
+			{
+				id: 1,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 2,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 3,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 4,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 5,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 6,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 7,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 8,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 9,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+			{
+				id: 10,
+				title: 'Fancy catchy unique title',
+				desc: 'words that are describing the video that will enrich your life. ',
+				imageURL: 'https://source.unsplash.com/random/?therapy',
+			},
+		],
+		promoVid: {
+			id: 11,
+			title: 'Main promotional video!',
+			desc: 'words that are describing the video that will enrich your life. ',
+			imageURL: 'https://source.unsplash.com/random/?therapy',
+			purchased: true,
+		},
+	};
+	const [state] = useReducer(videoReducer, initialState);
+
 	// todo add functions for fetching, adding, and removing videos
 
 	const value = {
-		// todo set all states and functions to be exported here
-	}
-	return (
-		<VideoContext.Provider value={value}>
-			{children}
-		</VideoContext.Provider>
-	)
-}
+		videos: state.videos,
+		promoVid: state.promoVid,
+	};
+	return <VideoContext.Provider value={value}>{children}</VideoContext.Provider>;
+};
 
-export default VideoState
+export default VideoState;
