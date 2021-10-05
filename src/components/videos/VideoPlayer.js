33bd90888b659c2d529fda842deab646ff401042
Linkinlog <dahlton@linkd.dev>
2021-10-04 21:24:54 -0400

    Videos: refactored to use embedded video

--- src/components/videos/VideoPlayer.js
+++ src/components/videos/VideoPlayer.js
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Card, Col } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import { Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import VideoContext from '../../context/video/videoContext';
+import EmbeddedVideo from './EmbeddedVideo';
+import { SingleVideo } from './SingleVideo';
 const VideoPlayer = () => {
 	const videoContext = useContext(VideoContext);
 	const { videos } = videoContext;
@@ -14,18 +16,9 @@ const VideoPlayer = () => {
 	}, [id, videos]);
 	return (
 		<Col key={video?.id} className='my-4'>
-				<Card className='pb-1 text-center shadow1 rounded rounded-3'>
-					<iframe src="https://www.youtube.com/embed/EVAQWRqL6LY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					{video?.isPurchased && (
-						<Card.ImgOverlay className='locked'>
-							<h2>Not purchased yet</h2>
-						</Card.ImgOverlay>
-					)}
-					<Card.Body>
-						<Card.Title>{video?.title}</Card.Title>
-						<Card.Text>{video?.desc?.substring(0, 40)}...</Card.Text>
-					</Card.Body>
-				</Card>
+			{video?.isPurchased ?
+				<EmbeddedVideo video={video} /> : <SingleVideo video={video} />
+		}
 		</Col>
 	);
 };
